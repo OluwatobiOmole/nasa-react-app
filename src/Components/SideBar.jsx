1a5162e6cdf8734bde8d29fcd68ae35e35a359ff
Tobi Omole <D22125039@mytudublin.ie>
2024-12-03 16:11:03 +0000

    CSS style changes and sideBar jsx changes

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,10 +1,16 @@
 export default function SideBar() {
   return (
-    <div>
-        <h2> The Brutal Martian Landscape</h2>
-        <div>
-            <p>Description</p>
-            <p>snd nfkjehfiow enjfwhefhiwhe jeohfouiwehfu oihdedfu</p>
+    <div  className="sidebar">
+        <div className="bgOverlay"></div>
+        <div className="sidebarContents">
+            <h2> The Brutal Martian Landscape</h2>
+            <div>
+                <p>Description</p>
+                <p>snd nfkjehfiow enjfwhefhiwhe jeohfouiwehfu oihdedfu</p>
+            </div>
+            <button>
+              <i className="fa-solid fa-right-long"></i>
+            </button>
         </div>
     </div>
   )
